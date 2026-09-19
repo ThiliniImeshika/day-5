bfd6f8afb1cf54bad81beee65616ef5ef77a92ea
thiliniimeshika <thiliniimeshika2003@gmail.com>
2026-09-20 00:10:01 +0530

    operators

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// comparison operators
+
+// let x = 11;
+// let y = "11";
+
+// console.log(x == y);
+
+// console.log(x === y);
+
+//logical operations
+
+if(!false){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
