76f70674ca86f5788e3f3cf420b73f51ee3817b8
thiliniimeshika <thiliniimeshika2003@gmail.com>
2026-09-20 00:18:20 +0530

    loops

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
-// Arithmetic operators
+//for loop
 
-// let x = 10;
-// let y = 20;
-
-// let z = x / y;
-
-// console.log(z);
-
-// comparison operators
-
-// let x = 11;
-// let y = "11";
-
-// console.log(x == y);
-
-// console.log(x === y);
-
-//logical operations
-
-if(!false){
-    console.log("true");
+for(let i = 0; i < 5; i++) {
+    console.log(i);
 }
-else{
-    console.log("false");
+
+
+//while loop
+let i = 0;
+while(i <= 10) {
+    console.log(i);
+    i++;
 }
+
+//do while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+}while(j <= 10);
